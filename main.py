14c901280f9831ad73491b4f1fbad2e187efbd9b
Alex Celis <celis@Alexs-MacBook-Pro.local>
2022-05-22 12:34:56 -0400

    updated get request

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ import json
 
 from data_func import items_api #this is my own class for dealing with the data
 
+
+
 app = Flask(__name__)
+
+items_func = items_api()
+
 
 #This is the main page
 @app.route("/")
@@ -21,31 +26,26 @@ def hello_world():
 @app.route("/items",  methods=['GET','POST'])
 def my_items():
     '''
-    This is the items page that we will be editing through the api. 
-
-    TODO - define the scope of the items and their interactions as an api. current prompt scope:
-            The API includes endpoints on the `/items` resource that allow a client to retrieve the
-            current items, set the list of items, and delete all of the items
-    TODO - create post request to add items to the list.
-    TODO - use json as the data that its recieving/sending.
+    This is the items page that we will be editing through the api. This is an endpoint that 
+    communicates with my items_api class.
     
     '''
 
     #TODO - this function will be an endpoint but all data functions and request function will be done through another class
-
     if request.method == "GET":
         #-- only use this to look for data 
 
         #placeholder for more meaningful data
         # print(request.data , '--inc value')
 
-        data = request.args.get('my_value') #this is how i will be recieving the data
-        print(data)
+        # action = request.args.get('action') #this is how i will be recieving the data
+        id = int(request.args.get('id'))
+        name = request.args.get('name')
+        print(id,name)
+        data = items_func.get_data(id,name)
 
 
-
-        return jsonify({'id': '111111',
-                       'name': data})
+        return jsonify(data)
 
 
 
@@ -54,15 +54,18 @@ def my_items():
     if request.method == "POST":
         #-- use this to modify delete
         
-        print(request)
+        # print(request)
 
         
         data = request.get_json() #this is how i will be recieving the data
-        print(data)
+        # print(data)
 
         other_data = request.get_json() #this is how i will be recieving the data
        
-        print(other_data)
+        # print(other_data)
+
+        print('recieving values::')
+        print(request.values)
 
 
         return jsonify({'id': data,
