8256052701e2e0f1111c1746f7c40a9284a99efe
Alex Celis <celis@Alexs-MacBook-Pro.local>
2022-05-22 21:37:41 -0400

    simplified request pings

--- access_api.py
+++ access_api.py
@@ -10,23 +10,20 @@ Initiale testing of accessing the API
 website_name = "http://localhost:3000"
 
 
-#get with payload params
-payload = {'my_value': 'some_name'}
-r = requests.get(website_name + "/items", params=payload)
+# -- get function testing
+# payload = {'my_value': 'some_name'}
+# r = requests.get(website_name + "/items", params=payload)
+# print(r.text)
 
+#--Manual testing of the api
+temp_order = {"action": "get", "id": 3 , "name": "suzy"}
+temp2 = {"action": "delete_all", "id": 4 , "name": "johny"}
+temp3 = {"action": "post", "id": 123 , "name": "herold"}
+# r = requests.get(website_name + "/items", params=temp_order)
+r = requests.post(website_name + "/items", json=temp3)
 print(r.text)
 
-#get request 
-r = requests.get(website_name + "/items") #wow
-
+r = requests.post(website_name + "/items", json=temp3)
 print(r.text)
 
 
-#post request
-r = requests.post(website_name + "/items" ,json={'my_value': 'post_name'})
-print(r.text)
-
-
-#post request with payload params
-r = requests.post(website_name + "/items", json={"other_val": "test_name"})
-print(r.text)
